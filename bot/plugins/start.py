339c1b733d258b15326863caffd5cd9d00e3002f
newylbot <mimik1sudosu@gmail.com>
2025-02-26 18:23:20 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,4 +1,5 @@
 from pyrogram import filters as Filters
+from pyrogram.enums import ChatAction
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 from ..translations import Messages as tr
@@ -13,7 +14,7 @@ from ..utubebot import UtubeBot
     & Filters.user(Config.AUTH_USERS)
 )
 async def _start(c: UtubeBot, m: Message):
-    await m.reply_chat_action("typing")
+    await m.reply_chat_action(ChatAction.TYPING)
 
     await m.reply_text(
         text=tr.START_MSG.format(m.from_user.first_name),
