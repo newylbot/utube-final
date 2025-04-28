8fc29e0daf14ecced8bfdd18a0bd2a6720d8e015
newylbot <mimik1sudosu@gmail.com>
2025-04-28 22:11:53 +0530

    Added Status Command

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,6 +1,8 @@
 from pyrogram import filters as Filters
 from pyrogram.enums import ChatAction
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+import os
+import platform
 
 from ..translations import Messages as tr
 from ..config import Config
@@ -21,5 +23,35 @@ async def _start(c: UtubeBot, m: Message):
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton("Updated the code by ๏ ʟᴜᴍɪɴᴏ ⇗ ˣᵖ", url="https://t.me/itz_lumino")]]
+        ),
+    )
+
+
+@UtubeBot.on_message(
+    Filters.private
+    & Filters.incoming
+    & Filters.command(["status", "st"])
+    & Filters.user(Config.AUTH_USERS)
+)
+async def _status(c: UtubeBot, m: Message):
+    await m.reply_chat_action(ChatAction.TYPING)
+
+    auth_status = "✅ Logged In" if os.path.exists("auth_token.txt") else "❌ Not Logged In"
+
+    text = f"""
+<b>🤖 Bot Status:</b>
+
+<b>Authentication:</b> {auth_status}
+<b>Allowed Users:</b> {len(Config.AUTH_USERS)}
+<b>Bot Version:</b>  ʀᴇx - ᴅ  <b>v28.04.2025</b>
+<b>Python Version:</b> {platform.python_version()}
+<b>Server:</b> {'Heroku' if os.getenv('DYNO') else '๏ ʟᴜᴍɪɴᴏ ⇗ ˣᵖ'}
+
+"""
+    await m.reply_text(
+        text=text,
+        quote=True,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("๏ Support ⇗", url="https://t.me/itz_lumino")]]
         ),
     )
